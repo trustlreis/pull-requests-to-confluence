d8fcf46bc203e48216490aeb2d675735222b7333
Luis Machado Reis <luis.reis@trustly.com>
2024-06-17 10:21:01 -0300

    fixing Dockerfile build issues

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,8 @@
 # Start the cron service
 service cron start
 
+# Creating file if it not exists
+touch /var/log/cron.log
+
 # Tail the cron log to keep the container running
 tail -f /var/log/cron.log
